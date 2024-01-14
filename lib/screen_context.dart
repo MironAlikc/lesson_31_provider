ebebf70321d375d9f4ee928be4b1ea8b790fad25
Aleksandr Myroniuk <miron.alikc@gmail.com>
2024-01-14 03:56:47 -0800

    commit

--- lib/screen_context.dart
+++ lib/screen_context.dart
@@ -92,3 +92,19 @@ class MyText2 extends StatelessWidget {
     );
   }
 }
+
+class Name extends InheritedWidget {
+  const Name({super.key, required this.child}) : super(child: child);
+
+  final Widget child;
+
+  static Name? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Name>();
+  }
+
+  @override
+  bool updateShouldNotify(Name oldWidget) {
+    return true;
+    
+  }
+}
